6d01abda29c67c26cb851ca8dcd0b982866d7e97
Tomas Gustafson <Tomas Gustafson>
2016-03-15 15:58:16 -0700

    Initial commit, fix CSS and plugins.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(function() {
+	$('.bxslider').bxSlider({
+		speed: 450,
+		controls: false,
+		auto: true,
+		minSlides: 1,
+		maxSlides: 4,
+		slideMargin: 0
+	});
+
+	$(':submit').click(function(event) {
+		var inputText = $('#email').val();
+		if (inputText.length !== 0) {
+		alert('Thanks for subscribing!');
+		} else {
+			alert('Please add a valid email address.');
+		}
+	});
+});
